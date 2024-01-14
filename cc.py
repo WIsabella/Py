530279bab7449c1568837680abd0059153ab1c9f
smile-sdu-wjl <1849709067@qq.com>
2024-01-14 16:59:00 +0800

    daily

--- cc.py
+++ cc.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
-for i in range(1, 85):
-    if 168 % i == 0:
-        j = 168 / i;
-        if i > j and (i + j) % 2 == 0 and (i - j) % 2 == 0:
-            m = (i + j) / 2
-            n = (i - j) / 2
-            x = n * n - 100
-            print(x)
+print("Hello World! ")
+# 这是一个注释
+# 这又是一个注释
+'''
+print("Hello Python!")
+if True:
+    print ("True")
+else:
+    print ("False")
+# total = a + \
+#   b + \
+#    c
+print(r"this is a \n")
+'''
+'''
+word = '字符串'
+sentence = "这是一个句子。"
+paragraph = """这是一个段落，
+可以由多行组成"""
+'''
+# str='123456789'
+# print(str)
+# print(str[0:-1])
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str[1:5:2])
+# print(str*2)
+# print(str+'你好')
+# print('-------------------------------')
+# print('hello\npython')
+# print(r'hello\npython')
+# x='a'
+# y='b'
+# print(x)
+# print(y)
+# print('--------')
+# print(x,end=" ")
+# print(y,end=' ')
