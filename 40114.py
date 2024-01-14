b57275144fc28d9c9359eaee6b033d698a9bac36
smile-sdu-wjl <1849709067@qq.com>
2024-01-14 22:08:39 +0800

    daily

---
+++ 40114.py
@@ -0,0 +1,37 @@
+n=int(input())
+print(n)
+print(type(n))
+
+name = input()
+print('I am %s and I am studying Python in Nowcoder!'%name)
+
+number=input()
+n=float(number)
+print('%.2f'%n)
+
+a=float(input())
+print(int (a))
+
+num=input()
+num=int(num,16)
+print(num)
+
+name1 = input()
+name2 = input()
+print(name1+name2)
+
+print(len(input()))
+
+name=input()
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+name=input()
+print(name.strip())
+
+name=input()
+print(name*100)
+
+str=input()
+print(str[0:10])
